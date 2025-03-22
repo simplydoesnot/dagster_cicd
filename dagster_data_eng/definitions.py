89ec8c583919d076e6f5cdb72584234520df36ac
simplydoesnot <emorris.mxi@gmail.com>
2025-03-22 16:52:21 -0700

    Created the definitions.py file

---
+++ dagster_data_eng/definitions.py
@@ -0,0 +1,12 @@
+from dagster_data_eng.assets import people_asset
+
+import dagster as dg
+
+
+people_assets = dg.load_assets_from_package_module(people_asset, group_name="TESTING")
+
+
+
+defs = dg.Definitions(
+    assets= people_assets
+)
