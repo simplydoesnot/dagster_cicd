f14f9e878ce186183d461f21fd4e5a7184d7d5bd
simplydoesnot <emorris.mxi@gmail.com>
2025-03-22 16:55:10 -0700

    Made changes to the file path.

--- dagster_data_eng/assets/people_asset/people_asset.py
+++ dagster_data_eng/assets/people_asset/people_asset.py
@@ -1,15 +1,16 @@
 import pandas as pd
 import dagster as dg
+from pathlib import Path
 
 @dg.asset
 def processed_data():
-    df = pd.read_csv("../raw_data/sample.csv")
+    df = pd.read_csv(Path(__file__).parent/"../raw_data/sample.csv")
     
     df["age_group"] = pd.cut(
         df["age"], bins = [0, 20, 40, 100], labels=["Young", "Middle", "Senior"]
     )
     
-    df.to_csv("../raw_data/processed_data.csv", index=False)
+    df.to_csv(Path(__file__).parent/"../raw_data/processed_data.csv", index=False)
     return "Data loaded successfully"
 
 
