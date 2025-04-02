19405497628d981ed9b81dc3873b1283cb4d90aa
simplydoesnot <emorris.mxi@gmail.com>
2025-04-02 01:35:44 -0700

    pushing linting workflow. Hope in Works

--- dagster_data_eng/assets/people_asset/people_asset.py
+++ dagster_data_eng/assets/people_asset/people_asset.py
@@ -8,7 +8,7 @@ def processed_data():
     df = pd.read_csv(Path(__file__).parent / "../raw_data/sample.csv")
 
     df["age_group"] = pd.cut(
-        df["age"], bins=[0, 20, 40, 100], labels=["Young", "Middle", "Senior"]
+                            df["age"], bins=[0, 20, 40, 100], labels=["Young", "Middle", "Senior"]
     )
 
     df.to_csv(Path(__file__).parent / "../raw_data/processed_data.csv", index=False)
