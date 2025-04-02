b6dba02960e3153f7cc3309d385ddc4b91552087
simplydoesnot <emorris.mxi@gmail.com>
2025-04-02 02:11:02 -0700

    Passing black and flake8

--- dagster_data_eng/assets/people_asset/people_asset.py
+++ dagster_data_eng/assets/people_asset/people_asset.py
@@ -5,11 +5,12 @@ from pathlib import Path
 
 @dg.asset
 def processed_data():
-    df = pd.read_csv(Path(__file__).parent / "../raw_data/sample.csv")
+    file_path = Path(__file__).parent / "../raw_data/sample.csv"
+    df = pd.read_csv(file_path)
 
     df["age_group"] = pd.cut(
-                df["age"], bins=[0, 20, 40, 100], labels=["Young", "Middle", "Senior"]
+        df["age"], bins=[0, 20, 40, 100], labels=["Young", "Middle", "Senior"]
     )
-
-    df.to_csv(Path(__file__).parent / "../raw_data/processed_data.csv", index=False)
+    dest_path = Path(__file__).parent / "../raw_data/processed_data.csv"
+    df.to_csv(dest_path, index=False)
     return "Data loaded successfully"
--- dagster_data_eng/definitions.py
+++ dagster_data_eng/definitions.py
@@ -3,7 +3,7 @@ from dagster_data_eng.assets import people_asset
 import dagster as dg
 
 
-people_assets = dg.load_assets_from_package_module(people_asset, group_name="TESTING")
+people_assets = dg.load_assets_from_package_module(people_asset)
 
 
 defs = dg.Definitions(assets=people_assets)
